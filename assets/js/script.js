48a34f7f789eccb20cc01c813d3c632c8e027836
DEVitor0 <vitormoreira6940@gmail.com>
2024-01-27 18:06:16 -0300

    aba de titulo sobre o curso

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const hamburgerBtn = document.getElementById('hamburger-btn');
 const menuList = document.querySelector('.menu-list');
 
-
 hamburgerBtn.addEventListener('click', function () {
   this.classList.toggle('active');
   menuList.classList.toggle('active');

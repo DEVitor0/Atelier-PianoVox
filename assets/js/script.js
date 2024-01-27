a9ef08d5e819510f18ba1d5f835f3fd561efb362
DEVitor0 <vitormoreira6940@gmail.com>
2024-01-27 16:52:54 -0300

    adição de logo, fonte e correção de alinhamento no menu

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const menuList = document.querySelector('.menu-list');
+
+
+hamburgerBtn.addEventListener('click', function () {
+  this.classList.toggle('active');
+  menuList.classList.toggle('active');
+});
